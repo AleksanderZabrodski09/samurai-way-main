41b419cda8d52907b977f08e9762f86a832f4ef6
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-05-25 00:38:15 +0200

    add 7-lesson of the way-s

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <ul>
-            <li>html</li>
-            <li>css</li>
-            <li>ts</li>
-            <li>react</li>
-        </ul>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>ts</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">New Feed</a>
+            <a href="#s">Message</a>
+        </div>
+    )
+}
+
 
 export default App;
