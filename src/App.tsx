a2020281b40ee98de840a4a0747634a14c15b170
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-06-17 12:58:33 +0200

    create: add 20-lesson of the way-s(add NavLink)

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,22 @@ import {Header} from './components/Header/Header';
 import {Navbar} from './components/Navbar/Navbar';
 import {Profile} from './components/Profile/Profile';
 import {Dialogs} from './components/Dialogs/Dialogs';
-import s from './components/Dialogs/Dialogs.module.css';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header/>
-      <Navbar/>
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header/>
+        <Navbar/>
         <div className="appWrapperContent">
-          <Route path='/dialogs' component={Dialogs}/>
-          <Route path='/profile' component={Profile}/>
-          {/*<Profile/>*/}
-          {/*<Dialogs/>*/}
+          <Route path='./profile' component={Profile}/>
+          <Route path='./dialogs' component={Dialogs}/>
         </div>
-      </BrowserRouter>
-
-
-    </div>
-  );
+      </div>
+    </BrowserRouter>
+      )
 }
 
 
