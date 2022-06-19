8b9d99c67137a2599488f1c4223e0d6c5d46c933
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-06-20 00:25:13 +0200

    create: add 23-lesson(create ProfileInfo)

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './ProfileInfo.module.css';
+
+
+export const ProfileInfo = () => {
+  return (
+    <div>
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMPihIju0BBCwq0HWOFF-aZo8vlz2FCOl7fg&usqp=CAU"
+             alt="image"/>
+      </div>
+      <div className={s.descriptionBlock}>ava+desc.</div>
+    </div>
+  );
+};
+
