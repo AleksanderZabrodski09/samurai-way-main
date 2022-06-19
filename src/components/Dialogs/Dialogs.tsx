1a5ebd991ae5603923cb3124adea4cef37d5bb75
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-06-19 16:17:48 +0200

    create: add 21-lesson of the way-s(Make up the Dialogs page)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 import s from './Dialogs.module.css';
 
 
-
 export const Dialogs = () => {
   return (
-    <div >
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Robert
+        </div>
+        <div className={s.dialog}>
+         Andrzej
+        </div>
+        <div className={s.dialog}>
+          Pawel
+        </div>
+        <div className={s.dialog}>
+          Marek
+        </div>
+        <div className={s.dialog}>
+          Bob
+        </div>
+        <div className={s.dialog}>
+         Jack
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How are you going?</div>
+        <div className={s.message}>i am fine</div>
+
+      </div>
     </div>
+
   );
 };
 
