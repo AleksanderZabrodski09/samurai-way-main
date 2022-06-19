26d2f6975063dc9b09b01c6177b2bb46734dfd85
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-06-19 17:31:33 +0200

    change: add NavLink Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,19 +7,19 @@ export const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <NavLink to='/profile'>Profile</NavLink>
+        <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to='/dialogs'>Messages</NavLink>
+        <NavLink to='/dialogs' activeClassName={s.active}>Messages</NavLink>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href='/news'>News</a>
+        <NavLink to='/news' activeClassName={s.active}>News</NavLink>
       </div>
       <div className={s.item}>
-        <a href='/music'>Music</a>
+        <NavLink to='/music' activeClassName={s.active}>Music</NavLink>
       </div>
       <div className={s.item}>
-        <a href='/settings'>Settings</a>
+        <NavLink to='/settings' activeClassName={s.active}>Settings</NavLink>
       </div>
     </nav>
   )
