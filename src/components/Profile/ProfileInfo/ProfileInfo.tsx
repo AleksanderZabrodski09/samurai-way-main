85351437606e5a8832bfa533ae1807c999f0e2fb
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-07-05 20:38:25 +0200

    change: add ProfileInfo

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const ProfileInfo = () => {
+  return (
+    <div>
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMPihIju0BBCwq0HWOFF-aZo8vlz2FCOl7fg&usqp=CAU"
+             alt="image"/>
+      </div>
+      <div>ava+desc.</div>
+    </div>
+  );
+};
+
